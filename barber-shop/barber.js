79e9b91faa48332fcea0bdbad7f55a1a8ac13a93
Yuji Kosakowski <yuji.kosakowski@gmail.com>
2023-07-30 22:33:21 -0600

    Add function to barber.js

--- barber-shop/barber.js
+++ barber-shop/barber.js
@@ -1,3 +1,34 @@
+function createBarber(fname, fearnings = 0, fhaircuts = []) {
+  return {
+    name: fname,
+    earnings: fearnings,
+    haircuts: fhaircuts
+  };
+}
 
+function giveCompliment(cutType) {
+  return "This "+cutType['style']+" looks great!"
 
-module.exports = {  };
+}
+
+function cutHair(barber, cutType) {
+  barber.haircuts.push(cutType);
+  barber.earnings += cutType.price 
+  return barber;
+}
+function listStyles(barber, style) {
+  cuts = []
+  for (var i = 0; i < barber.haircuts.length; i++) {
+    if (barber.haircuts[i].hairLength == style) {
+      cuts.push(barber.haircuts[i].style)
+    }
+  }
+  return cuts
+}
+
+module.exports = {
+  createBarber,
+  giveCompliment,
+  cutHair,
+  listStyles
+  };
